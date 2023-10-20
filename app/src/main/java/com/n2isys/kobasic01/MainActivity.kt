c98923786f7e462a8c32b584c714718fd408ea16
Daljung <msfall@hanmail.net>
2023-10-20 16:17:20 +0900

    viewBinding 선언해주기

--- app/src/main/java/com/n2isys/kobasic01/MainActivity.kt
+++ app/src/main/java/com/n2isys/kobasic01/MainActivity.kt
@@ -2,11 +2,18 @@ package com.n2isys.kobasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.n2isys.kobasic01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding // 요거 선언도 해줘야함 _01
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater,null,false)  // 요거 해줘야함 _02
+        setContentView(binding.root) // 요거 해줘야함 _03 binding.root
+
+
+
 
 
 
