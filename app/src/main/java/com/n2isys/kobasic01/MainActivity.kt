762ad72b07420739020a29c1612afca88782cde1
Daljung <msfall@hanmail.net>
2023-10-21 10:51:29 +0900

    로그찍어보기

--- app/src/main/java/com/n2isys/kobasic01/MainActivity.kt
+++ app/src/main/java/com/n2isys/kobasic01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.n2isys.kobasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.n2isys.kobasic01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater,null,false)  // 요거 해줘야함 _02
         setContentView(binding.root) // 요거 해줘야함 _03 binding.root
 
+        binding.clickBtn.setOnClickListener {
 
+            Log.d( "메인화면에서 찍는로그", "클릭용 버튼 눌림")
+
+
+
+
+        }
 
 
 
